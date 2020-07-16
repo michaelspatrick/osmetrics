1d143de444cdd97290324dbe50ad396e22e100b2
Mike Patrick <45206934+toritejutsu@users.noreply.github.com>
2020-07-16 00:26:12 -0400

    Update getcpu.h

--- includes/getcpu.h
+++ includes/getcpu.h
@@ -5,9 +5,9 @@
 #include <stdbool.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <errno.h>
 
 struct procstat{
-  char id;
   int user;
   int nice;
   int sys;
@@ -15,21 +15,66 @@ struct procstat{
   int iowait;
   int irq;
   int softirq;
-  int guest;
-  int guest_nice;
+  int intr;
+  int ctxt;
+  int btime;
+  int processes;
+  int procs_running;
+  int procs_blocked;
+  int total_softirq;
+  int numcores;
+  float cpu_speed;
+  float cpu_bogomips;
 };
 
-//char id[16], dummy[16];
-
 procstat getprocstat(void) {
-  char *line = NULL;
+  char line[256];
   FILE* fp = fopen("/proc/stat", "r");
-  size_t len = 0;
   struct procstat ps;
+  char id[16], dummy[16];
 
-  getline(&line, &len, fp);
-  sscanf( line, "%s %d %d %d %d %d %d %d %d %d", &ps.id, &ps.user, &ps.nice, &ps.sys, &ps.idle, &ps.iowait, &ps.irq, &ps.softirq, &ps.guest, &ps.guest_nice);
-  free(line);
+  while (fgets(line, sizeof(line), fp)) {
+    if (strstr(line, "cpu ")) {
+      sscanf( line, "%s %s %d %d %d %d %d %d %d", dummy, id, &ps.user, &ps.nice, &ps.sys, &ps.idle, &ps.iowait, &ps.irq, &ps.softirq);
+    } else if (strstr(line, "intr")) {
+      sscanf( line, "%s %d", dummy, &ps.intr);
+    } else if (strstr(line, "ctxt")) {
+      sscanf( line, "%s %d", dummy, &ps.ctxt);
+    } else if (strstr(line, "btime")) {
+      sscanf( line, "%s %d", dummy, &ps.btime);
+    } else if (strstr(line, "processes")) {
+      sscanf( line, "%s %d", dummy, &ps.processes);
+    } else if (strstr(line, "procs_running")) {
+      sscanf( line, "%s %d", dummy, &ps.procs_running);
+    } else if (strstr(line, "procs_blocked")) {
+      sscanf( line, "%s %d", dummy, &ps.procs_blocked);
+    } else if (strstr(line, "softirq")) {
+      sscanf( line, "%s %d", dummy, &ps.total_softirq);
+    }
+  }
+
   fclose(fp);
+  
+    // Read /proc/cpuinfo
+  fp = fopen("/proc/cpuinfo", "r");
+  assert(fp != NULL);
+  char *token;
+  while (fgets(line, sizeof(line), fp)) {
+    if (strstr(line, "cpu cores")) {
+      token = strtok(line, ":");
+      token = strtok(NULL, ": ");
+      ps.numcores = atoi(token);
+    } else if (strstr(line, "cpu MHz")) {
+      token = strtok(line, ":");
+      token = strtok(NULL, ": ");
+      ps.cpu_speed = atof(token);
+    } else if (strstr(line, "bogomips")) {
+      token = strtok(line, ":");
+      token = strtok(NULL, ": ");
+      ps.cpu_bogomips = atof(token);
+    }
+  }
+  fclose(fp);
+
   return ps;
 }
